b18e59d4a5a91c0a85067014d2af19dfe45852d4
Kerim1030 <kaplanerkerim891@gmail.com>
2026-02-13 16:12:22 +0100

    Exercise 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -17,8 +17,17 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+
+
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   const handleClick = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
@@ -29,6 +38,8 @@ const App = () => {
     <div>
       {anecdotes[selected]}
       <br/>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={handleVote} text='vote' />
       <Button handleClick={handleClick} text='next anecdote' />
     </div>
   )
