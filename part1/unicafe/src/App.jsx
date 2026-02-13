96eb151fb083bb82e892f1fd3ed39f5884280d04
Kerim1030 <kaplanerkerim891@gmail.com>
2026-02-13 15:29:31 +0100

    Exercice 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,10 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text, value }) => (
+  <p>{text} {value}</p>
+)
+
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   if (all === 0) {
     return (
@@ -20,12 +24,13 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   return (
     <div>
     <h2>statistics</h2>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {all}</p>
-    <p>average {average}</p>
-    <p>positive {positive} %</p>
+    <StatisticLine text='good' value={good} />
+    <StatisticLine text='neutral' value={neutral} />
+    <StatisticLine text='bad' value={bad} />
+    <StatisticLine text='all' value={all} />
+    <StatisticLine text='average' value={average} />
+    <StatisticLine text='positive' value={`${positive} %`} />
+    
     </div>
   )
     
