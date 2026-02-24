66a8033a82f64b8f7fa0d17551906f303635dd51
Kerim1030 <kaplanerkerim891@gmail.com>
2026-02-24 11:53:39 +0100

    Exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -31,23 +34,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={showFilter} onChange={(event) => setShowFilter(event.target.value)} />
-      </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {showAll.map(person => <p key={person.name}>{person.name}: {person.number}</p>)}
+      <Filter showFilter={showFilter} setShowFilter={setShowFilter} />
+      <h3>Add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
+      <h3>Numbers</h3>
+      <Persons showAll={showAll} />
     </div>
   )
 }
