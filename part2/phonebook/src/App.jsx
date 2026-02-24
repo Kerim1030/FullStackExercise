c39854f9a7d16b5fdbc9d6011545a92d3d694c50
Kerim1030 <kaplanerkerim891@gmail.com>
2026-02-24 11:36:16 +0100

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')  
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -13,10 +14,12 @@ const App = () => {
       return
     }
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -27,11 +30,14 @@ const App = () => {
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name}: {person.number}</p>)}
     </div>
   )
 }
