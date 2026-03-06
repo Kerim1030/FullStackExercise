f94bdfebe039885071342c2b095aa5df6083eaa1
Kerim1030 <kaplanerkerim891@gmail.com>
2026-03-06 15:05:11 +0100

    Exercise 2.20

--- part2/countries/src/components/OneCountry.jsx
+++ part2/countries/src/components/OneCountry.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const OneCountry = ({ country }) => {
+  const [meteo, setMeteo] = useState(null)
+  const capitale = country.capital ? country.capital[0] : null
+
+  useEffect(() => {
+    
+    if (capitale) {
+      const api_key = import.meta.env.VITE_SOME_KEY
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${capitale}&appid=${api_key}&units=metric`
+
+      axios
+        .get(url)
+        .then(response => {
+          setMeteo(response.data)
+        })
+        .catch(error => {
+          console.log("Erreur de récupération météo :", error)
+        })
+    }
+  }, [capitale])
+
       return (
         <div>
           <h2>{country.name.common}</h2>
@@ -16,6 +40,15 @@ const OneCountry = ({ country }) => {
             alt={`Flag of ${country.name.common}`} 
             width="150" 
           />
+
+          {meteo && (
+            <div>
+              <h3>Weather in {capitale}</h3>
+              <p>temperature {meteo.main.temp} Celcius</p>
+             
+              <p>wind {meteo.wind.speed} m/s</p>
+            </div>
+          )}
         </div>
       )
 }
