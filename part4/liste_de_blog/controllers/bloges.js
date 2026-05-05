7d57ba5ded83579cc3c20013c3705011cc5f8d31
Kerim1030 <kaplanerkerim891@gmail.com>
2026-05-05 15:42:17 +0200

    Exercice 4.8 + 4.9 + 4.10 +  4.11 + 4.12

--- part4/liste_de_blog/controllers/bloges.js
+++ part4/liste_de_blog/controllers/bloges.js
@@ -1,17 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/bloges')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-  blog.save().then(result => {
-    response.status(201).json(result)
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.title || !request.body.url) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes
   })
+
+  const saved = await blog.save()
+  response.status(201).json(saved)
 })
 
 module.exports = blogsRouter
---
+++ part4/liste_de_blog/tests/blog_api.test.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/bloges')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+describe('GET /api/blogs', () => {
+  test('les blogs sont retournés en JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('le bon nombre de blogs est retourné', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+})
+
+describe('format des blogs', () => {
+  test('la propriété id existe et pas _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+    expect(blog.id).toBeDefined()
+    expect(blog._id).toBeUndefined()
+  })
+})
+
+describe('POST /api/blogs', () => {
+  test('un nouveau blog est bien ajouté', async () => {
+    const newBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    const titles = response.body.map(b => b.title)
+    expect(titles).toContain('Canonical string reduction')
+  })
+
+  test('likes vaut 0 par défaut si non fourni', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html'
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    expect(response.body.likes).toBe(0)
+  })
+
+  test('400 si title manquant', async () => {
+    const newBlog = {
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com',
+      likes: 3
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
+
+  test('400 si url manquante', async () => {
+    const newBlog = {
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      likes: 0
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
